a81e3a0b4e8209d3ccff6f956be9dc97a8696904
kang <>
2026-06-07 00:24:23 +0900

    広告配置の設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,6 +81,8 @@ export default function Home() {
     <main className={styles.main}>
       <head>
         <meta name="google-adsense-account" content="ca-pub-1722119470458198"></meta>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1722119470458198"
+     crossOrigin="anonymous"></script>
       </head>
       {/* ヘッダー */}
       <header className={styles.header}>
